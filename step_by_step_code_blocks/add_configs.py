dd3383fe9b11493393225e125c7c0d1f8c6da715
AlphaVantageSupport <74164855+AlphaVantageSupport@users.noreply.github.com>
2021-05-01 18:04:10 -0400

    Create add_configs.py

---
+++ step_by_step_code_blocks/add_configs.py
@@ -0,0 +1,34 @@
+config = {
+    "alpha_vantage": {
+        "key": "YOUR_API_KEY", # Claim your free API key here: https://www.alphavantage.co/support/#api-key
+        "symbol": "IBM",
+        "outputsize": "full",
+        "key_adjusted_close": "5. adjusted close",
+    },
+    "data": {
+        "window_size": 20,
+        "train_split_size": 0.80,
+    }, 
+    "plots": {
+        "xticks_interval": 90, # show a date every 90 days
+        "color_actual": "#001f3f",
+        "color_train": "#3D9970",
+        "color_val": "#0074D9",
+        "color_pred_train": "#3D9970",
+        "color_pred_val": "#0074D9",
+        "color_pred_test": "#FF4136",
+    },
+    "model": {
+        "input_size": 1, # since we are only using 1 feature, close price
+        "num_lstm_layers": 2,
+        "lstm_size": 32,
+        "dropout": 0.2,
+    },
+    "training": {
+        "device": "cpu", # "cuda" or "cpu"
+        "batch_size": 64,
+        "num_epoch": 100,
+        "learning_rate": 0.01,
+        "scheduler_step_size": 40,
+    }
+}
