2e7b031f5727bf946bcd756d4f151cb6c2b2d1d2
AlphaVantageSupport <74164855+AlphaVantageSupport@users.noreply.github.com>
2021-05-01 18:10:33 -0400

    Create predict_future_prices.py

---
+++ step_by_step_code_blocks/predict_future_prices.py
@@ -0,0 +1,40 @@
+# predict the closing price of the next trading day
+
+model.eval()
+
+x = torch.tensor(data_x_unseen).float().to(config["training"]["device"]).unsqueeze(0).unsqueeze(2) # this is the data type and shape required, [batch, sequence, feature]
+prediction = model(x)
+prediction = prediction.cpu().detach().numpy()
+
+# prepare plots
+
+plot_range = 10
+to_plot_data_y_val = np.zeros(plot_range)
+to_plot_data_y_val_pred = np.zeros(plot_range)
+to_plot_data_y_test_pred = np.zeros(plot_range)
+
+to_plot_data_y_val[:plot_range-1] = scaler.inverse_transform(data_y_val)[-plot_range+1:]
+to_plot_data_y_val_pred[:plot_range-1] = scaler.inverse_transform(predicted_val)[-plot_range+1:]
+
+to_plot_data_y_test_pred[plot_range-1] = scaler.inverse_transform(prediction)
+
+to_plot_data_y_val = np.where(to_plot_data_y_val == 0, None, to_plot_data_y_val)
+to_plot_data_y_val_pred = np.where(to_plot_data_y_val_pred == 0, None, to_plot_data_y_val_pred)
+to_plot_data_y_test_pred = np.where(to_plot_data_y_test_pred == 0, None, to_plot_data_y_test_pred)
+
+# plot
+
+plot_date_test = data_date[-plot_range+1:]
+plot_date_test.append("tomorrow")
+
+fig = figure(figsize=(25, 5), dpi=80)
+fig.patch.set_facecolor((1.0, 1.0, 1.0))
+plt.plot(plot_date_test, to_plot_data_y_val, label="Actual prices", marker=".", markersize=10, color=config["plots"]["color_actual"])
+plt.plot(plot_date_test, to_plot_data_y_val_pred, label="Past predicted prices", marker=".", markersize=10, color=config["plots"]["color_pred_val"])
+plt.plot(plot_date_test, to_plot_data_y_test_pred, label="Predicted price for next day", marker=".", markersize=20, color=config["plots"]["color_pred_test"])
+plt.title("Predicted close price of the next trading day")
+plt.grid(b=None, which='major', axis='y', linestyle='--')
+plt.legend()
+plt.show()
+
+print("Predicted close price of the next trading day:", round(to_plot_data_y_test_pred[plot_range-1], 2))
