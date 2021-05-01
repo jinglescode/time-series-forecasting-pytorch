7b05be0271f95170ed0f5d0c2242bc5bbe56ea15
AlphaVantageSupport <74164855+AlphaVantageSupport@users.noreply.github.com>
2021-05-01 18:04:58 -0400

    Create get_market_data.py

---
+++ step_by_step_code_blocks/get_market_data.py
@@ -0,0 +1,30 @@
+def download_data(config):
+    ts = TimeSeries(key=config["alpha_vantage"]["key"])
+    data, meta_data = ts.get_daily_adjusted(config["alpha_vantage"]["symbol"], outputsize=config["alpha_vantage"]["outputsize"])
+
+    data_date = [date for date in data.keys()]
+    data_date.reverse()
+
+    data_close_price = [float(data[date][config["alpha_vantage"]["key_adjusted_close"]]) for date in data.keys()]
+    data_close_price.reverse()
+    data_close_price = np.array(data_close_price)
+
+    num_data_points = len(data_date)
+    display_date_range = "from " + data_date[0] + " to " + data_date[num_data_points-1]
+    print("Number data points", num_data_points, display_date_range)
+
+    return data_date, data_close_price, num_data_points, display_date_range
+
+data_date, data_close_price, num_data_points, display_date_range = download_data(config)
+
+# plot
+
+fig = figure(figsize=(25, 5), dpi=80)
+fig.patch.set_facecolor((1.0, 1.0, 1.0))
+plt.plot(data_date, data_close_price, color=config["plots"]["color_actual"])
+xticks = [data_date[i] if ((i%config["plots"]["xticks_interval"]==0 and (num_data_points-i) > config["plots"]["xticks_interval"]) or i==num_data_points-1) else None for i in range(num_data_points)] # make x ticks nice
+x = np.arange(0,len(xticks))
+plt.xticks(x, xticks, rotation='vertical')
+plt.title("Daily close price for " + config["alpha_vantage"]["symbol"] + ", " + display_date_range)
+plt.grid(b=None, which='major', axis='y', linestyle='--')
+plt.show()
