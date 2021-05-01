e4285d958efce96aaa10179441019009fe52a6f4
AlphaVantageSupport <74164855+AlphaVantageSupport@users.noreply.github.com>
2021-05-01 18:09:05 -0400

    Create model_training.py

---
+++ step_by_step_code_blocks/model_training.py
@@ -0,0 +1,47 @@
+def run_epoch(dataloader, is_training=False):
+    epoch_loss = 0
+
+    if is_training:
+        model.train()
+    else:
+        model.eval()
+
+    for idx, (x, y) in enumerate(dataloader):
+        if is_training:
+            optimizer.zero_grad()
+
+        batchsize = x.shape[0]
+
+        x = x.to(config["training"]["device"])
+        y = y.to(config["training"]["device"])
+
+        out = model(x)
+        loss = criterion(out.contiguous(), y.contiguous())
+
+        if is_training:
+            loss.backward()
+            optimizer.step()
+
+        epoch_loss += (loss.detach().item() / batchsize)
+
+    lr = scheduler.get_last_lr()[0]
+
+    return epoch_loss, lr
+
+train_dataloader = DataLoader(dataset_train, batch_size=config["training"]["batch_size"], shuffle=True)
+val_dataloader = DataLoader(dataset_val, batch_size=config["training"]["batch_size"], shuffle=True)
+
+model = LSTMModel(input_size=config["model"]["input_size"], hidden_layer_size=config["model"]["lstm_size"], num_layers=config["model"]["num_lstm_layers"], output_size=1, dropout=config["model"]["dropout"])
+model = model.to(config["training"]["device"])
+
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=config["training"]["learning_rate"], betas=(0.9, 0.98), eps=1e-9)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=config["training"]["scheduler_step_size"], gamma=0.1)
+
+for epoch in range(config["training"]["num_epoch"]):
+    loss_train, lr_train = run_epoch(train_dataloader, is_training=True)
+    loss_val, lr_val = run_epoch(val_dataloader)
+    scheduler.step()
+    
+    print('Epoch[{}/{}] | loss train:{:.6f}, test:{:.6f} | lr:{:.6f}'
+              .format(epoch+1, config["training"]["num_epoch"], loss_train, loss_val, lr_train))
