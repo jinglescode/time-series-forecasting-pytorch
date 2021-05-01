e048c34711aaf8aba23b3288590f1bbbc4c70682
AlphaVantageSupport <74164855+AlphaVantageSupport@users.noreply.github.com>
2021-05-01 18:05:28 -0400

    Create normalize_input_data.py

---
+++ step_by_step_code_blocks/normalize_input_data.py
@@ -0,0 +1,17 @@
+class Normalizer():
+    def __init__(self):
+        self.mu = None
+        self.sd = None
+
+    def fit_transform(self, x):
+        self.mu = np.mean(x, axis=(0), keepdims=True)
+        self.sd = np.std(x, axis=(0), keepdims=True)
+        normalized_x = (x - self.mu)/self.sd
+        return normalized_x
+
+    def inverse_transform(self, x):
+        return (x*self.sd) + self.mu
+
+# normalize
+scaler = Normalizer()
+normalized_data_close_price = scaler.fit_transform(data_close_price)
